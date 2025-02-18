5e9a1858968ce694c395e9b2b0e6c3365782cee1
CutTheWire <sjmbee04@gmail.com>
2025-02-18 10:41:39 +0900

    Merge breanch 'train' into develop

--- fastapi/src/main.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-from utils.Database_mongo import MongoDBHandler
-
-async def main():
-    db_handler = MongoDBHandler()
-    await db_handler.insert_dataset('fastapi/datasets/HAERAE-HUB_KOREAN-WEBTEXT_train.json', 'dataset')
-
-if __name__ == "__main__":
-    asyncio.run(main())
